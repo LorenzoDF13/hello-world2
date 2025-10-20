78606641e8fb7c69175967af7a1b7232e5451788
lorenzo <difilippolorenzo0@gmail.com>
2025-10-20 10:32:05 +0200

    commit fork

--- src/saluto.java
+++ src/saluto.java
@@ -1,5 +1,5 @@
 public class saluto {
     public static void main(String[] args) {
-        System.out.println("ciao mondo bellissimo");
+        System.out.println("ciao mondo bellissimo!");
     }
 }
